bab133746da24e0a0ce11c0ecda5a6d4c9f84dac
TanishaAgarwall <102983755+TanishaAgarwall@users.noreply.github.com>
2022-05-04 15:05:42 +0530

    Add files via upload

---
+++ studentt.py
@@ -0,0 +1,29 @@
+import csv
+def write_into_csv(info_list):
+    with open('student_info.csv','w+',newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        if csv_file.tell() == 0:
+            writer.writerow(["Name","Age","Contact number","E-Mail ID"])
+        writer.writerow(info_list)
+if __name__ == '__main__':
+    condition = True
+    student_num = 1
+    
+    while(condition):
+        student_info=input("Enter student information for student #{} in the following format (Name Age Contact Number E-mail_ID): "
+                           .format(student_num)) 
+        # split
+        student_info_list = student_info.split(' ')
+        print('\n The entered information is -\nName:{} \nAge: {}\nContact Number: {}\n Email ID:{}'
+              .format(student_info_list[0],student_info_list[1],student_info_list[2],student_info_list[3]))
+        choice_check = input("Is the entered information correct?(yes/no):")
+        if choice_check == "yes":
+            write_into_csv(student_info_list)
+            condition_check=input ("Enter (yes/no) if you want to enter information for another student: ")
+            if condition_check=="yes":
+                condition = True
+                student_num = student_num +1
+            elif condition_check=="no":
+                condition = False  
+        elif choice_check == 'no':
+            print("\n Please re-enter the values!")
